ff1253e62f94bb8e9cf5ee6047fd91921542a4cf
liuwenai <liu97106@163.com>
2020-04-06 09:56:19 +0800

    新增

--- moveZero.js
+++ moveZero.js
@@ -1,14 +1,31 @@
 function moveZero(arr) {
-    let array = arr.sort(function (a, b) {
-        return a - b
-    })
-    array.forEach(element => {
-        if (element === 0) { 
-            array.shift()
-            array.push(0) 
+    let len = arr.length  // 5
+    let j = 0
+    for(let i = 0;i < len-j;i++){                         
+        if(arr[i] === 0){
+            arr.push(0)       //在最后一位后增加一个0
+            arr.splice(i,1)   //从第i位开始删除一个元素  
+            i--               //重新开始                        
+            j++              
         }
-    });
-    console.log(array)
+    }
+    return arr
 }
-let arr = [4, 7, 3, 8, 0, 0, 0]
-moveZero(arr)
+let arr = [0,1,0,0,3,12]
+console.log(moveZero(arr))
+
+
+//splice() 从第0位（第一个）开始删除两个元素，并增加'','',''
+// var myFish = ['angel', 'clown', 'trumpet', 'sturgeon'];
+// var removed = myFish.splice(0, 2, 'parrot', 'anemone', 'blue');
+// console.log(removed)
+
+//作用域
+function foo(a) {
+    if (typeof a === "number" || a instanceof Number) {
+        const b = a + 1
+        console.log(b)
+    }
+}
+
+foo(12)
--- 例题3.js
+++ 例题3.js
@@ -70,3 +70,10 @@ let arryn = ['1,3,2','1,89,37,4','1,4,2']
 let m = arryn.toString().split(",").sort((a,b)=>{ return a-b}).map(Number) 
 let n = [... new Set(m)]   // 去重
 console.log(n)
+
+
+//parseInt() 函数解析一个字符串参数，并返回一个指定基数的整数 (数学系统的基础)。
+//const intValue = parseInt(string[, radix]);
+//string 要被解析的值。如果参数不是一个字符串，则将其转换为字符串(使用 ToString 抽象操作)。字符串开头的空白符将会被忽略。
+//radix 一个介于2和36之间的整数(数学系统的基础)，表示上述字符串的基数。默认为10。 返回值 返回一个整数或NaN
+console.log(['1','2','3'].map(parseInt))
