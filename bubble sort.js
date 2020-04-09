de5e19460d5dbe199f5587e92fd71c1a0641d9b7
liuwenai <liu97106@163.com>
2020-04-09 16:54:52 +0800

    调整

--- bubble sort.js
+++ bubble sort.js
@@ -14,3 +14,27 @@ function bubbleSort(arr){
 }
 var arr = [2,7,1,5,11,67,21]
 console.log(bubbleSort(arr));
+
+
+//改进
+
+//每次最大值放到最右后，会将本轮最后一个操作的位置作为下一轮的终点，可以减少不必要的一些冒泡
+
+function bubbleSort1(arr){
+    let i = arr.length - 1
+    while(i > 0){
+        let pos = 0
+        for(let j = 0; j < i;j++){
+            if(arr[j] > arr[j + 1]){
+                pos = j
+                const temp = arr[j]
+                arr[j] = arr[j + 1]
+                arr[j + 1] = temp
+            }
+        }
+        i = pos
+    }
+    console.log(arr)
+}
+
+bubbleSort1(arr)
