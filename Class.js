b866ded86a5558ad08be17e48619c08638a1fd57
liuwenai <liu97106@163.com>
2020-04-14 15:54:44 +0800

    实现一个类

--- Class.js
+++ Class.js
@@ -1,58 +1,135 @@
-class LazyManClass{
-    constructor(name){
-        this.taskList = []
-        this.name = name
-        console.log(`Hi I am ${this.name}`)
-        setTimeout(() =>{
-            this.next()
-        },1000)
-    }
-    eat(name){
-        var that = this;
-        var fn = (function (n){
-            return function (){
-                console.log(`I am eating ${n}`)
-                that.next()
-            }
-        })(name)
-        this.taskList.push(fn)
-        return this
-    }
-    sleepFirst(time){
-        var that = this
-        var fn = (function (t){
-            return function(){
-                setTimeout(()=>{
-                    console.log(`等待了${t}秒`)
-                    that.next()
-                },t * 1000)
-            }
-        })(time)
-        this.taskList.push(fn)
-        return this
-    }
-    sleep(time){
-        var that = this 
-        var fn = (function (t){
-            return function(){
-                setTimeout(()=>{
-                    console.log(`等待了${t}秒`)
-                    that.next()
-                },t*1000)
-            }
-        })(time)
-        this.taskList.push(fn)
-        return this
-    }
-    next(){
-        var fn = this.taskList.shift()
-        fn && fn()
+// class LazyManClass{
+//     constructor(name){
+//         this.taskList = []
+//         this.name = name
+//         console.log(`Hi I am ${this.name}`)
+//         setTimeout(() =>{
+//             this.next()
+//         },1000)
+//     }
+//     eat(name){
+//         var that = this;
+//         var fn = (function (n){
+//             return function (){
+//                 console.log(`I am eating ${n}`)
+//                 that.next()
+//             }
+//         })(name)
+//         this.taskList.push(fn)
+//         return this
+//     }
+//     sleepFirst(time){
+//         var that = this
+//         var fn = (function (t){
+//             return function(){
+//                 setTimeout(()=>{
+//                     console.log(`等待了${t}秒`)
+//                     that.next()
+//                 },t * 1000)
+//             }
+//         })(time)
+//         this.taskList.push(fn)
+//         return this
+//     }
+//     sleep(time){
+//         var that = this 
+//         var fn = (function (t){
+//             return function(){
+//                 setTimeout(()=>{
+//                     console.log(`等待了${t}秒`)
+//                     that.next()
+//                 },t*1000)
+//             }
+//         })(time)
+//         this.taskList.push(fn)
+//         return this
+//     }
+//     next(){
+//         var fn = this.taskList.shift()
+//         fn && fn()
+//     }
+// }
+
+// function LazyMan(name){
+//     return new LazyManClass(name)
+// }
+
+// LazyMan('Tony').eat('lunch').eat('dinner').sleepFirst(5).sleep(4).eat('junk food');
+// LazyMan('Tony').eat('lunch').eat('dinner').sleepFirst(5).sleep(10).eat('junk food');
+
+
+// JavaScript实现一个类，实例化
+
+// 构造函数法 this + prototype
+
+function Mobile(name,price){
+    this.name = name
+    this.price = price
+}
+
+Mobile.prototype.sell = function(){
+    console.log(this.name + ',售价￥'+ this.price)
+}
+
+var iPhone = new Mobile('iPhone',4000)
+iPhone.sell()
+
+// Object.create法
+
+var Person = {
+    firsrname: "Mark",
+    lastname: "Yun",
+    age: 25,
+    introduce:function(){
+        console.log('I am ' + Person.firsrname + ' ' + Person.lastname)
     }
 }
 
-function LazyMan(name){
-    return new LazyManClass(name)
+var person = Object.create(Person)
+person.introduce()
+
+if(!Object.create){
+    Object.create = function(o){
+        function F(){}
+        F.prototype = o
+        return new F()
+    }
 }
 
-LazyMan('Tony').eat('lunch').eat('dinner').sleepFirst(5).sleep(4).eat('junk food');
-LazyMan('Tony').eat('lunch').eat('dinner').sleepFirst(5).sleep(10).eat('junk food');
+// 极简主义法
+
+var Cat = {
+    age: 3,
+    createNew: function(){
+        var cat = {}
+        cat.name = "lalala"
+        var sount = "maomaomaomao"
+        cat.makeSount = function(){
+            console.log(sount)
+        }
+        cat.changeAge = function(num){
+            Cat.age = num;
+            console.log(Cat.age)
+        }
+        return cat
+    }
+}
+
+var cat = Cat.createNew()
+console.log(cat.name)
+cat.makeSount()
+cat.changeAge(7)
+
+// 语法糖
+ class Point{
+     constructor(x,y){
+         this.x = x
+         this.y = y
+     }
+     toString(){
+         console.log('(' + this.x + ',' + this.y +')')
+     }
+ }
+
+ var point = new Point(2,3)
+ point.toString()
--- README.md
+++ README.md
@@ -93,11 +93,13 @@ ______
 ##### parseInt() 函数解析一个字符串参数，并返回一个指定基数的整数 (数学系统的基础)。
 ###### const intValue = parseInt(string[, radix]);
 > string 要被解析的值。如果参数不是一个字符串，则将其转换为字符串(使用 ToString 抽象操作)。字符串开头的空白符将会被忽略。
+>
 > radix 一个介于2和36之间的整数(数学系统的基础)，表示上述字符串的基数。默认为10。 返回值 返回一个整数或NaN
 
 
 ##### sort()方法用于对数组的元素进行排序。默认排序顺序为按字母升序。
 > 升序: XXX.sort(function(a,b){return a-b});
+>
 > 降序: XXX.sort(function(a,b){return b-a});
 
 
