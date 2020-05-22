2d502357b38e45dae008a044b10b381d49ba8618
liuwenai <liu97106@163.com>
2020-05-22 17:29:15 +0800

    调整

--- 例题3.js
+++ 例题3.js
@@ -1,8 +1,8 @@
 
 // 求两个数组的交集
 
-let arr1 = [1]
-let arr2 = [1, 1]
+let arr1 = [1,1,4]
+let arr2 = [1, 1,1]
 let arr = [];
 for (let i = 0; i < arr1.length; i++) {
     for (let j = 0; j < arr2.length; j++) {
@@ -32,49 +32,52 @@ console.log(arr)
 // console.log(result)
 
 
-let mupltipy8 = (num) => num << 3
-console.log(mupltipy8(7))
+// let mupltipy8 = (num) => num << 3
+// console.log(mupltipy8(7))
 
-function bitAdd(m, n) {
-    while (m) {
-        [m, n] = [(m & n) << 1, m ^ n];
-    }
-    return n;
-}
-function multiply7(num) {
-    let sum = 0;
-    for (var i = 0; i < 7; i++) {
-        sum = bitAdd(sum, num);
-    }
-    return sum;
+// function bitAdd(m, n) {
+//     while (m) {
+//         [m, n] = [(m & n) << 1, m ^ n];
+//     }
+//     return n;
+// }
+// function multiply7(num) {
+//     let sum = 0;
+//     for (var i = 0; i < 7; i++) {
+//         sum = bitAdd(sum, num);
+//     }
+//     return sum;
 
-}
-console.log(multiply7(7)); //49
-
-
-let str='aaadwiubcggkiwj';
-const arry = str.match(/(\w)\1+/g);                // 求出连续字符串
-console.log(arry)
-const maxLen = Math.max(...arry.map(s => s.length));  //求出最长长度
-console.log(maxLen)
-const result = arry.reduce((pre, curr) => {
-  if (curr.length === maxLen) {
-    pre[curr[0]] = curr.length;
-  }
-  return pre;
-}, {});
-console.log(result);
+// }
+// console.log(multiply7(7)); //49
 
 
-let arryn = ['1,3,2','1,89,37,4','1,4,2']
-let m = arryn.toString().split(",").sort((a,b)=>{ return a-b}).map(Number) 
-console.log(m)
-let n = [... new Set(m)]   // 去重
-console.log(n)
+// let str='aaadwiubcggkiwj';
+// const arry = str.match(/(\w)\1+/g);                // 求出连续字符串
+// console.log(arry)
+// const maxLen = Math.max(...arry.map(s => s.length));  //求出最长长度
+// console.log(maxLen)
+// const result = arry.reduce((pre, curr) => {
+//   if (curr.length === maxLen) {
+//     pre[curr[0]] = curr.length;
+//   }
+//   return pre;
+// }, {});
+// console.log(result);
 
 
-//parseInt() 函数解析一个字符串参数，并返回一个指定基数的整数 (数学系统的基础)。
-//const intValue = parseInt(string[, radix]);
-//string 要被解析的值。如果参数不是一个字符串，则将其转换为字符串(使用 ToString 抽象操作)。字符串开头的空白符将会被忽略。
-//radix 一个介于2和36之间的整数(数学系统的基础)，表示上述字符串的基数。默认为10。 返回值 返回一个整数或NaN
-console.log(['1','2','3'].map(parseInt))
+// let arryn = ['1,3,2','1,89,37,4','1,4,2']
+// let m = arryn.toString().split(",").sort((a,b)=>{ return a-b}).map(Number) 
+// console.log(m)
+// let n = [... new Set(m)]   // 去重
+// console.log(n)
+
+
+// //parseInt() 函数解析一个字符串参数，并返回一个指定基数的整数 (数学系统的基础)。
+// //const intValue = parseInt(string[, radix]);
+// //string 要被解析的值。如果参数不是一个字符串，则将其转换为字符串(使用 ToString 抽象操作)。字符串开头的空白符将会被忽略。
+// //radix 一个介于2和36之间的整数(数学系统的基础)，表示上述字符串的基数。默认为10。 返回值 返回一个整数或NaN
+// console.log(['1','2','3'].map(parseInt))
+
+
+
